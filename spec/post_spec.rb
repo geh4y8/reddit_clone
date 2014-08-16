23d4b2590aa5d6290a6f74d32c5980737037b70b
geh4y8 <guy.halperin88@gmail.com>
2014-08-16 13:26:50 -0700

    update spec coverage

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -15,4 +15,26 @@ describe 'Post' do
     posts = Post.today(Date.today)
     expect(posts).to eq [post, post2]
   end
+
+  it 'grabs all posts by the user in chron order' do
+    user = User.create(name: 'Guy', password: 'green235235')
+    user2 = User.create(name: 'OtherGuy', password: 'reallylongpassword')
+    post = Post.create(description: 'awesome reddit post', user_id: user.id, date: Date.today)
+    post2 = Post.create(description: 'next awesome reddit post', user_id: user.id, date: Date.today)
+    post3 = Post.create(description: 'next awesome reddit post by user 2', user_id: user2.id, date: Date.today)
+    posts = Post.where(user_id: user.id).order(:date)
+    expect(posts).to eq [post, post2]
+  end
+
+  it 'grabs all the five newest posts' do
+    user = User.create(name: 'Guy', password: 'green235235')
+    post1 = Post.create(description: 'awesome reddit post', user_id: user.id, date: Date.today)
+    post2 = Post.create(description: 'next awesome reddit post', user_id: user.id, date: Date.today)
+    post3 = Post.create(description: 'next best reddit post', user_id: user.id, date: Date.today)
+    post4 = Post.create(description: 'next next best awesome reddit post', user_id: user.id, date: Date.today)
+    post5 = Post.create(description: 'almost last reddit post', user_id: user.id, date: Date.today)
+    post6 = Post.create(description: 'last reddit post', user_id: user.id, date: Date.today)
+    posts = Post.order(date: :asc).limit(5)
+    expect(posts).to eq [post1,post2,post3,post4,post5]
+  end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'User' do
-  it 'creates a new user with a name' do
+  it 'creates a new user with a name and password' do
     user = User.create(name: 'Guy', password: 'green47388')
     expect(user).to be_a User
   end
